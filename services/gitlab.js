e3b823b82ed6e63d9c62f0fc7cae1e576cacb177
Anand Chowdhary <mail@anandchowdhary.com>
2019-01-03 20:49:09 +0530

    Add promise resolving

--- services/gitlab.js
+++ services/gitlab.js
@@ -1,6 +1,12 @@
 const request = require("request");
 const dayjs = require("dayjs");
 
+const promiseSerial = funcs =>
+	funcs.reduce(
+		(promise, func) => promise.then(result => func().then(Array.prototype.concat.bind(result))),
+		Promise.resolve([])
+	);
+
 const yesterday = "2018-12-20";
 const today = dayjs().format("YYYY-MM-DD");
 
@@ -28,7 +34,35 @@ module.exports = (req, res) => {
 					}
 				});
 			} catch (e) {}
+			let index = 1;
+			const githubCommits = commits.map(commit => () =>
+				new Promise((resolve, reject) => {
+					request(
+						{
+							uri: `https://anandchowdhary.com`
+						},
+						(error, response, body) => {
+							console.log(
+								`(${index}/${commits.length}) ${
+									commit.message.split("GitLab commit: ")[1].split(" ")[0]
+								}`
+							);
+							index++;
+							if (error) return reject();
+							resolve();
+						}
+					);
+				})
+			);
 			res.json(commits);
+
+			promiseSerial(githubCommits)
+				.then(() => {
+					console.log("Completed!");
+				})
+				.catch(error => {
+					console.log("Error!", error);
+				});
 		}
 	);
 };
