e364736601618e8a528933d30bb9ffb90a23c5e4
Anand Chowdhary <mail@anandchowdhary.com>
2019-01-03 13:26:58 +0530

    Add list of commits

--- services/gitlab.js
+++ services/gitlab.js
@@ -1,15 +1,34 @@
 const request = require("request");
+const dayjs = require("dayjs");
+
+const yesterday = "2018-12-20";
+const today = dayjs().format("YYYY-MM-DD");
 
 module.exports = (req, res) => {
 	request(
 		{
-			uri: `https://gitlab.elnino.tech/api/v4/events?action=pushed&after=2018-12-20&before=2019-01-03&private_token=${
+			uri: `https://gitlab.elnino.tech/api/v4/events?action=pushed&after=${yesterday}&before=${today}&private_token=${
 				process.env.GITLAB_TOKEN
 			}`
 		},
 		(error, response, body) => {
-			res.json(["done", body]);
-			console.log(error, body);
+			const commits = [];
+			try {
+				body = JSON.parse(body);
+				body.forEach(push => {
+					if (push.push_data) {
+						for (let i = 0; i < push.push_data.commit_count; i++) {
+							commits.push({
+								message: `GitLab commit: ${push.push_data.commit_to} in ${
+									push.project_id
+								} by ${push.author.name} (@${push.author.username})`,
+								time: dayjs(push.created_at).add(i, "minutes")
+							});
+						}
+					}
+				});
+			} catch (e) {}
+			res.json(commits);
 		}
 	);
 };
