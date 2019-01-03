3b718dfcbb9a3894e94367042037ac75d9a1deea
Anand Chowdhary <mail@anandchowdhary.com>
2019-01-03 22:19:39 +0530

    Add CRON

--- crons/day.js
+++ crons/day.js
@@ -1,10 +1,24 @@
 module.exports = () => {
-    // Run wayback daily
-    require("../services/wayback")({
-        body: {}
-    }, {
-        json: text => {
-            console.log(text);
-        }
-    });
+	// Run wayback daily
+	require("../services/wayback")(
+		{
+			body: {}
+		},
+		{
+			json: text => {
+				console.log(text);
+			}
+		}
+	);
+	// Run GitLab daily
+	require("../services/gitlab")(
+		{
+			body: {}
+		},
+		{
+			json: text => {
+				console.log(text);
+			}
+		}
+	);
 };
